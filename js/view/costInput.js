5c549fa2802090930c1375fd13a6a15e9e7c58e6
Daniar02 <abykeev02@icloud.com>
2023-03-27 22:17:04 +0600

    Calculator: The cost of real estate. inputCost - View.

---
+++ js/view/costInput.js
@@ -0,0 +1,14 @@
+
+function init(getData) {
+    const input = document.querySelector('#input-cost');
+
+    const settings = {
+        numeral: true,
+        numeralThousandsGroupStyle: 'thousand',
+        delimiter: ' '
+    }
+
+    new Cleave(input, settings);
+}
+
+export default init;
