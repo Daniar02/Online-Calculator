975deeda631409cc733e961edc81263cd36fc1cf
Daniar02 <abykeev02@icloud.com>
2023-03-28 06:07:37 +0600

    Calculator: The cost of real estate. inputCost - Errors.

--- js/view/costInput.js
+++ js/view/costInput.js
@@ -8,7 +8,34 @@ function init(getData) {
         delimiter: ' '
     }
 
-    new Cleave(input, settings);
+    const cleaveInput = new Cleave(input, settings);
+    cleaveInput.setRawValue(getData().cost);
+
+    input.addEventListener('input', function () {
+        const value = +cleaveInput.getRawValue();
+        console.log(value);
+
+        // Проверка мин и макс цену
+        if (value < getData().minPrice || value > getData().maxPrice) {
+            input.closest('.param__details').classList.add('param__details--error');
+        }
+        if (value > getData().minPrice && value < getData().maxPrice) {
+            input.closest('.param__details').classList.remove('param__details--error');
+        }
+    });
+
+    input.addEventListener('change', function () {
+        const value = +cleaveInput.getRawValue();
+
+        if (value > getData().maxPrice) {
+            input.closest('.param__details').classList.add('param__details--error');
+            cleaveInput.setRawValue(getData().maxPrice);
+        }
+        if (value < getData().minPrice) {
+            input.closest('.param__details').classList.add('param__details--error');
+            cleaveInput.setRawValue(getData().minPrice);
+        }
+    })
 }
 
 export default init;
